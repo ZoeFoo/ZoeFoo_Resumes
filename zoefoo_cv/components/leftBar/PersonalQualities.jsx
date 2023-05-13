f4295fc23706783af3212c1fbc5b1baf5fdedc6c
Zoe Foo <foochormei335205@gmail.com>
2023-05-13 16:57:16 +0800

    add leading-loose each done style components & finished style Language component

--- zoefoo_cv/components/leftBar/PersonalQualities.jsx
+++ zoefoo_cv/components/leftBar/PersonalQualities.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const PersonalQualities = ({ characterList }) => {
     const characters = characterList.split('\n').map((character, i) => <div key={i}>{character}</div>);
     return (
-        <div className='text-center'>
-            <div className='grid'>
+        <div className='text-center leading-loose'>
+            <div className='grid'
+                style={{ letterSpacing: 1 }}>
                 <div className='title text-decoration w-5/6 text-xl pb-2 mb-4'>PERSONAL QUALITIES</div>
             </div>
 
